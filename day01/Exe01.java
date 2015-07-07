26781df3e67b0fc98fe3f75c1449142651009b9f
程子昂 <2792876175@qq.com>
2015-07-07 18:06:10 +0800

    the first work
    第一天的任务

---
+++ day01/Exe01.java
@@ -0,0 +1,20 @@
+package com.geiminuo;
+
+public class Exe01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+boolean a1=true;
+System.out.println(a1);
+boolean a2=false;
+System.out.println(a2);
+	boolean a3=0;
+System.out.println(a3);
+
+
+	}
+
+}
---
+++ day01/HelloChengZiang.java
@@ -0,0 +1,14 @@
+package com.geiminuo;
+
+public class HelloChengZiang {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String name="ChengZiang";
+System.out.println("Hello"+name);
+	}
+
+}
